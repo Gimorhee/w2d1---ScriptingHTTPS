5162a3defaa3ef936bf7a66128dcb59fc62ca5aa
Danny <dongyunrhee@gmail.com>
2019-04-08 18:44:38 +0000

    Step 2: Priting with buffered strings exercise.

---
+++ printWithBuffer2.js
@@ -0,0 +1,23 @@
+var https = require("https");
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+ /* Add your code here */
+
+  https.get(requestOptions, function(response){
+    response.setEncoding('utf8');
+
+    let data = "";
+    response.on('data', function(chunk){
+      data += chunk;
+      console.log("Chunks: " + chunk + '\n');
+    });
+  });
+}
+
+getAndPrintHTML();
